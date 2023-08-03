5c1ea0fb3a63df90f7e281f44b3e11aa1db724d9
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-08-03 13:16:32 +0300

    Adding upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,4 @@
-var exec = require("child_process").exec;
-
-function start(response) {
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
     
     var body = '<html>'+
@@ -21,10 +19,10 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("You've sent: " + postData);
     response.end();
 }
 
