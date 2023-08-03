87480b057f58345130dc6da364cf53ca636f84ab
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-08-03 13:24:21 +0300

    Handling POST

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,15 @@
+var querystring = require("querystring");
+
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
     
     var body = '<html>'+
       '<head>'+
-      '<meta http-quiv="Content-Type" content="text/html; '+
+      '<meta http-equiv="Content-Type" content="text/html; '+
       'charset=UTF-8" />'+
       '</head>'+
       '<body>'+
-      '<form action="/upload" method="post" />'+
+      '<form action="/upload" method="post">'+
       '<textarea name="text" rows="20" cols="60"></textarea>'+
       '<inpput type="submit" value="Submit text" />'+
       '</form>'+
@@ -22,7 +24,8 @@ function start(response, postData) {
 function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent: " + postData);
+    response.write("You've sent the text: "+
+    querystring.parse(postData).text);
     response.end();
 }
 
